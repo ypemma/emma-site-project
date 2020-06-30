9dcf054ff5e047c3d768a29096e2b55198b234a6
ypemma <63932016+ypemma@users.noreply.github.com>
2020-06-30 15:45:23 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         class="button is-primary"
         @click="$eventBus.$emit('modal-triggered', 'newsletter-modal')"
       >
-        Subscribe To Newsletter
+        Subscribe
       </button>
     </site-hero>
     <main-section theme="one-column">
